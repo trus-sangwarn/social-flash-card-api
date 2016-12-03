c18b4320abc8c7652d3bdd765967f2d77adb79f1
Trus Sangwarn <trus.sangwarn@icloud.com>
2016-12-03 16:54:33 +0700

    fix Headers allow

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const AccessToken = require('./app/models/access-token');
 
 app.all("/api/*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, X-Access-Token");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   return next();
 });
